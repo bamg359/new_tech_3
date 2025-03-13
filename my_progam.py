23b61acad280c793b1d8b0c812f7fadbf7c074bd
Ber Mira <bamg359@gmail.com>
2025-03-12 20:27:42 -0500

    Create ask function

--- my_progam.py
+++ my_progam.py
@@ -6,8 +6,13 @@ def calculatePrice(cost, revenue):
     print (price)
 
 
-calculatePrice(15000, 0.30)   
+#calculatePrice(15000, 0.30)   
 
 
+def ask_values():
+    cost = float(input("Enter the cost of the product: "))
+    revenue = float(input("Enter the revenue of the product: "))
+    calculatePrice(cost, revenue)
 
+ask_values()    
 
